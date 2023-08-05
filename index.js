5c57db1891e84135eed357150ae562626c396018
Anh Tran Duc <anh.td1401@gmail.com>
2023-08-05 22:45:01 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express"),
+  app = express(),
+  port = process.env.PORT || 4000,
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+var mqtt = require("./src/mqtt/index");
+mongoose
+  .connect(
+    "mongodb+srv://datpham:khong123@cluster0.jsljmga.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => {
+    console.log("Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ origin: "*" }));
+var router = require("./src/router/index");
+router(app);
+app.use(() => {
+  mqtt;
+});
+app.use(function (req, res) {
+  res.status(404).send({ url: req.originalUrl + "not found" });
+});
+app.listen(port);
+console.log("RESTful API server started on: " + port);
